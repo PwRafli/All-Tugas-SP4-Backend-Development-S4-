67ba1101cbf75c2dcd26f5ffab19d89480cb9304
PwRafli <rafliefendymm@gmail.com>
2026-03-04 09:38:36 +0700

    Add files via upload

---
+++ Tugas Pertemuan 2 Data Base/express_app/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Server berjalan dengan baik 🚀");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
